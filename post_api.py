8c0b48f242f59faa1b9e2b4e3c8bb6dbaee76466
HiepDang <hiepdangvo@icloud.com>
2021-01-08 11:55:00 +0700

    transfer from local computer

--- post_api.py
+++ post_api.py
@@ -1,9 +1,11 @@
 from request_data import *
+from breakeven_price import *
 
 def post_breakevenprice():
     tickers = request_ticker_list()
     breakeven_price = dict(name='breakeven_price')
     for ticker in tickers:
         try:
-            breakeven_price[ticker] =
-
+            breakeven_price[ticker] = monte_carlo(ticker=ticker, graph='off')
+        except KeyError:
+            continue
