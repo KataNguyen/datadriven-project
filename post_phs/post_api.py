ea026a35f245b5516c488d4205161cc68da8a20d
dangvohiep <hiepdangvo@icloud.com>
2021-01-15 13:02:10 +0700

    post_breakeven_price

--- post_phs/post_api.py
+++ post_phs/post_api.py
@@ -2,19 +2,32 @@ from request_phs.request_data import *
 from breakeven_price.monte_carlo import monte_carlo
 
 def post_breakeven_price():
-    address = 'https://api.phs.vn/market/utilities.svc/PostBreakevenPrice'
 
-    tickers = ['AAA', 'ACB']
-    breakeven_price = dict()
-    for ticker in tickers:
+    address = 'https://api.phs.vn/market/Utilities.svc/PostBreakevenPrice'
+    #tickers = request_ticker_list()
+    tickers = ['AAA', 'ACB', 'HBC', 'CTD', 'VCB']
+    breakeven_price = pd.Series(index=tickers, name='price', dtype=float)
+    for ticker in breakeven_price.index:
         try:
-            breakeven_price[ticker] = monte_carlo(ticker=ticker, graph='off')
+            price = monte_carlo(ticker=ticker, graph='off')
+            if price < 10000:
+                breakeven_price.loc[ticker] \
+                    = '{:,.0f}'.format(round(price,-1))
+            elif 10000 <= price < 50000:
+                breakeven_price.loc[ticker] \
+                    = '{:,.0f}'.format(50 * round(price/50))
+            else:
+                breakeven_price.loc[ticker] \
+                    = '{:,.0f}'.format(round(price,-2))
         except KeyError:
             continue
 
-    json_str = json.dumps(breakeven_price)
-    r = requests.post(url=address, data=json_str,
+    json_str = breakeven_price.to_json()
+
+    r = requests.post(url=address,
+                      data=json_str,
                       headers={'content-type': 'application/json'})
 
-    json.loads(r.json())
+    result = pd.DataFrame(json.loads(r.json()))
+
 
