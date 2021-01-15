d243a63df935957e8cb4a32ace505a8a0960742d
dangvohiep <hiepdangvo@icloud.com>
2021-01-15 19:54:54 +0700

    post_breakeven_price

--- credit_rating/kmeans_interpolation(jsc)-ver2.py
+++ credit_rating/kmeans_interpolation(jsc)-ver2.py
@@ -328,7 +328,6 @@ for standard in standards:
 
 del df, df_xs
 
-############# run from here on Saturday
 radius_centers = pd.DataFrame(index=kmeans_index, columns=periods)
 for row in range(centers.shape[0]):
     for col in range(centers.shape[1]):
