aa10e8f5ece4e3a77d6bc73e226b6c29e396af1e
dangvohiep <hiepdangvo@icloud.com>
2021-01-19 17:39:58 +0700

    post_breakeven_price

--- post_phs/post_api.py
+++ post_phs/post_api.py
@@ -4,33 +4,26 @@ from breakeven_price.monte_carlo import monte_carlo
 def post_breakeven_price():
 
     address = 'https://api.phs.vn/market/Utilities.svc/PostBreakevenPrice'
-    #tickers = request_ticker_list()
     tickers = request_ticker_list()
-    breakeven_price = pd.Series(index=tickers, name='price', dtype=float)
-    for ticker in breakeven_price.index:
-        try:
-            price = monte_carlo(ticker=ticker, graph='off')
-            breakeven_price.loc[ticker] = price
-            if price < 10000:
-                breakeven_price.loc[ticker] \
-                    = '{:.0f}'.format(round(price,-1))
-            elif 10000 <= price < 50000:
-                breakeven_price.loc[ticker] \
-                    = '{:.0f}'.format(50 * round(price/50))
-            else:
-                breakeven_price.loc[ticker] \
-                    = '{:.0f}'.format(round(price,-2))
-        except KeyError:
-            continue
+    breakeven_price = dict(index=tickers, name='price', dtype='int32')
+    for ticker in tickers:
+        price = monte_carlo(ticker=ticker, graph='off')
+        breakeven_price[ticker] = price
+        if price < 10000:
+            breakeven_price[ticker] \
+                = '{:.0f}'.format(round(price,-1))
+        elif 10000 <= price < 50000:
+            breakeven_price[ticker] \
+                = '{:.0f}'.format(50 * round(price/50))
+        else:
+            breakeven_price[ticker] \
+                = '{:.0f}'.format(round(price,-2))
 
-    breakeven_price = breakeven_price.astype(dtype='float').to_dict()
     json_str = {'symbol': json.dumps(breakeven_price)}
     json_str = json.dumps(json_str, separators=(',', ':'))
     r = requests.post(url=address,
                       data=json_str,
                       headers={'content-type':
                                    'application/json; charset=utf-8'})
-
     print(r)
-
 
