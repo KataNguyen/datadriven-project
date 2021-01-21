1838a72531fabf8cfb22ea4b5d7c6144ab2bec4b
dangvohiep <hiepdangvo@icloud.com>
2021-01-21 14:19:46 +0700

    post_breakeven_price

--- post_phs/post_api.py
+++ post_phs/post_api.py
@@ -19,17 +19,20 @@ def post_breakeven_price() -> None:
         tickers = request_ticker(segment)
         breakeven_price = dict(index=tickers, name='price', dtype='int32')
         for ticker in tickers:
-            price = monte_carlo(ticker=ticker, graph='off')
-            breakeven_price[ticker] = price
-            if price < 10000:
-                breakeven_price[ticker] \
-                    = '{:.0f}'.format(round(price,-1))
-            elif 10000 <= price < 50000:
-                breakeven_price[ticker] \
-                    = '{:.0f}'.format(50 * round(price/50))
-            else:
-                breakeven_price[ticker] \
-                    = '{:.0f}'.format(round(price,-2))
+            try:
+                price = monte_carlo(ticker=ticker, graph='off')
+                breakeven_price[ticker] = price
+                if price < 10000:
+                    breakeven_price[ticker] \
+                        = '{:.0f}'.format(round(price,-1))
+                elif 10000 <= price < 50000:
+                    breakeven_price[ticker] \
+                        = '{:.0f}'.format(50 * round(price/50))
+                else:
+                    breakeven_price[ticker] \
+                        = '{:.0f}'.format(round(price,-2))
+            except KeyError:
+                pass
 
     json_str = {'symbol': json.dumps(breakeven_price)}
     json_str = json.dumps(json_str, separators=(',', ':'))
