99301d8872b463da3313ba0bbc027b12c10ecf29
dangvohiep <hiepdangvo@icloud.com>
2021-03-28 13:55:00 +0700

    post_breakeven_price

--- text_mining/websrapping.py
+++ text_mining/websrapping.py
@@ -1,52 +1,6 @@
 from phs import *
 from request_phs import *
 
-def tinnghiepvutochucphathanh_bs4(num_hours:int=48):
-
-    url = 'https://vsd.vn/vi/alo/-f-_bsBS4BBXga52z2eexg'
-
-    num_hours = 48
-    now = datetime.now()
-
-    page_content = []
-    time_news = []
-    link_list = []
-    headline_list = []
-
-    fromtime = now
-    num_page = 0
-    while fromtime >= now + timedelta(hours=-num_hours):
-
-        num_page += 1
-
-        data = {'CurrentPage': 'changePage(9)'}
-        page = requests.post(url, data=data)
-
-        soup = BeautifulSoup(page.text, features='html.parser')
-
-        page_tag = soup.body.div.main.div.div.div.div.ul
-        page_li_list = page_tag.find_all('li')
-
-        for tag in page_li_list:
-            page_content += tag.find('h3')
-            time_news += tag.find('div')
-
-        for tag in page_content:
-            link_list += [tag['href']]
-            headline_list += [tag.string]
-
-        fromtime_str = time_news[-1][-21:]
-        fromtime = datetime.strptime(fromtime_str, '%d/%m/%Y - %H:%M:%S')
-
-        print(url)
-        print(time_news[-1][-21:])
-        print(fromtime_str)
-        print(now + timedelta(hours=-num_hours))
-
-
-###################### RETRY WITH SELENIUM ######################
-
-
 class vsd:
     def __init__(self):
         pass
@@ -65,9 +19,16 @@ class vsd:
         now = datetime.now()
         fromtime = now
 
-        news_headlines = []
-        news_urls = []
         news_time = []
+        news_headlines = []
+        news_exchanges =[]
+        news_tickers = []
+        news_dividends = []
+        news_ratios = []
+        news_recordsdate = []
+        news_paymentdate = []
+        news_reasons =[]
+        news_urls = []
 
         while fromtime >= now - timedelta(hours=num_hours):
             tags = driver.find_elements_by_xpath('/html/body/div/main'
@@ -79,18 +40,33 @@ class vsd:
                 news_headlines += [h3_tag.text]
                 news_urls += [h3_tag.find_element_by_tag_name('a')
                                   .get_attribute('href')]
-                news_time += [tag_.find_element_by_tag_name('div').text]
+                news_time_ = tag_.find_element_by_tag_name('div').text
+                news_time_ \
+                    = datetime.strptime(news_time_[-21:], '%d/%m/%Y - %H:%M:%S')
+                news_time += [news_time_]
 
-            fromtime_str = news_time[-1][-21:]
-            fromtime = datetime.strptime(fromtime_str, '%d/%m/%Y - %H:%M:%S')
+            fromtime = news_time[-1]
 
             # Turn Page
             page_buttons = driver.find_elements_by_xpath('/html/body/div/main/'
                                                          'div/div/div/div/'
                                                          'div/div/div/button')
-            page_buttons = page_buttons[-2]
-            page_buttons.click()
+            next_page = page_buttons[-2]
+            next_page.click()
 
+        output_table = pd.DataFrame(columns=['Thời gian', 'Sàn Giao Dịch',
+                                             'Cổ Phiếu', 'Tiêu Đề', 'Cổ tức',
+                                             'Tỷ lệ thực hiện',
+                                             'Ngày đăng ký cuối cùng',
+                                             'Ngày thanh toán',
+                                             'Lý do, mục đích'])
+        output_table['Time'] = news_time
+
+        output = pd.DataFrame(data={'Headline': news_headlines,
+                                    'Links': news_urls},
+                              index=news_time)
+
+vsd = vsd()
 
 class hnx:
     def __init__(self):
